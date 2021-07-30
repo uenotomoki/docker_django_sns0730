e650f11540fcfb4989ba8adb914441573860aa0f
uenotomoki <gifucom17@gmail.com>
2021-07-30 17:05:58 +0900

    make it better

--- default_allauth/settings.py
+++ default_allauth/settings.py
@@ -142,7 +142,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 
-"""
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -176,4 +175,3 @@ DATABASES['default'].update(db_from_env)
 #heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-"""
